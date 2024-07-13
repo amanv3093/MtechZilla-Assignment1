4b6e2e8adabcb8c023b0faf805e4c27b4104c1e7
aman verma <amanv3093@gmai.com>
2024-07-13 21:07:58 +0530

    update

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -21,7 +21,7 @@ function Header() {
 
       <div
         onClick={handleLogout}
-        className="cursor-pointer relative inline-flex items-center justify-start py-3 pl-4 pr-12 overflow-hidden font-semibold text-[#3498DB] transition-all duration-150 ease-in-out rounded hover:pl-10 hover:pr-6 bg-[#3f3e3e] group"
+        className="cursor-pointer relative inline-flex items-center justify-start py-3 pl-4 pr-12 overflow-hidden font-semibold text-[#3498DB] transition-all duration-150 ease-in-out rounded hover:pl-10 hover:pr-6 bg-[#494c4e] group"
       >
         <span className="absolute bottom-0 left-0 w-full h-1 transition-all duration-150 ease-in-out bg-[#3498DB] group-hover:h-full"></span>
         <span className="absolute right-0 pr-4 duration-200 ease-out group-hover:translate-x-12">
--- src/component/Timer/Timer.jsx
+++ src/component/Timer/Timer.jsx
@@ -52,32 +52,41 @@ const Timer = () => {
   return (
     <div>
       <Header />
-      <div className="timer-container mt-16 w-full flex items-center justify-center text-white">
-        <div className="h-[300px] w-[500px] flex flex-col justify-center items-center bg-[#3f3e3e] px-10 py-4 rounded-lg">
-          <h1 className="text-3xl font-bold">
-            {isFiveActive ? "Break Time" : "Running Time"}
-          </h1>
-          <h1 className="text-4xl text-center font-bold border w-[170px] py-1 rounded-md mt-6">
-            {time(timeLeft)}
-          </h1>
-          <div className="flex gap-4 mt-6">
+      <div className=" timer-container mt-16 w-full flex items-center justify-center text-white">
+        <div className="flex flex-col items-center justify-center min-w-[350px] bg-[#414345] text-text p-[2.5em_2em] rounded-[50px] shadow-[0_2.8px_2.2px_rgba(0,0,0,0.034),0_6.7px_5.3px_rgba(0,0,0,0.048),0_12.5px_10px_rgba(0,0,0,0.06),0_22.3px_17.9px_rgba(0,0,0,0.072),-2px_17px_33.4px_rgba(0,0,0,0.086),1px_7px_20px_2px_rgba(0,0,0,0.12)]">
+          <h1 className="text-2xl font-semibold pb-6">Pomodoro Timer</h1>
+          <div
+            style={{ boxShadow: "0px 0px 20px 0px hsla(0,0%,100%,0.2)" }}
+            className="flex flex-col items-center  rounded-full p-8 w-[180px] h-[180px]"
+          >
+            <h1 className="text-xl font-bold">
+              {isFiveActive ? "Break" : "Session"}
+            </h1>
+            <h1 className="mb-[0.3em] text-[50px] font-normal z-10">
+              {time(timeLeft)}
+            </h1>
+          </div>
+          <div className="flex gap-4 pt-14">
             <button
               onClick={startTimer}
               disabled={isRunning}
-              class="bg-[#3498DB] text-white border-none text-xl font-bold px-7 py-2 rounded-[6px] cursor-pointer shadow-md transition duration-200"
+              style={{ boxShadow: "0px 0px 20px 0px hsla(0,0%,100%,0.2)" }}
+              class="bg-[#414345] text-white border-none text-xl font-bold px-7 py-2 rounded-[6px] cursor-pointer shadow-md transition duration-200"
             >
               Start
             </button>
             <button
               onClick={pauseTimer}
               disabled={!isRunning}
-              class="bg-[#3498DB] text-white border-none text-xl font-bold px-7 py-2 rounded-[6px] cursor-pointer shadow-md transition duration-200"
+              style={{ boxShadow: "0px 0px 20px 0px hsla(0,0%,100%,0.2)" }}
+              class="bg-[#414345] text-white border-none text-xl font-bold px-7 py-2 rounded-[6px] cursor-pointer shadow-md transition duration-200"
             >
               Pause
             </button>
             <button
               onClick={resetTimer}
-              class="bg-[#3498DB] text-white border-none text-xl font-bold px-7 py-2 rounded-[6px] cursor-pointer shadow-md transition duration-200"
+              style={{ boxShadow: "0px 0px 20px 0px hsla(0,0%,100%,0.2)" }}
+              class="bg-[#414345] text-white border-none text-xl font-bold px-7 py-2 rounded-[6px] cursor-pointer shadow-md transition duration-200"
             >
               Reset
             </button>
